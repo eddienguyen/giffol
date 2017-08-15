29e1b34a2c9d864d76a80054542fb7101d654867
eddienguyen <eddienguyen996@gmail.com>
2017-08-15 20:05:35 +0700

    First Commit

---
+++ htdocs/index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+<link rel="stylesheet" type="text/css" href="css/style.css">
+
+</head>
+
+<body>
+
+	<div id="main">
+		<?php
+    		include("modules/header.php");
+        	include("modules/menu.php");
+	        include("modules/left.php");
+    	    include("modules/content.php");
+ 			include("modules/right.php");
+	        include("modules/footer.php");
+    	?>  
+    		
+    
+	</div>
+</body>
+</html>
