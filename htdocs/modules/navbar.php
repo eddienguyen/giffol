b3352d09e017eecb347493cdc554dc164f9f6454
eddienguyen <eddienguyen996@gmail.com>
2017-08-16 01:24:45 +0700

    add navbar and it's stylesheet(app.css

---
+++ htdocs/modules/navbar.php
@@ -0,0 +1,9 @@
+<div id="navbar">
+	<ul>
+		<li><a href="#">Get Start Now</a></li>
+		<li><a href="#">About</a></li>
+		<li><a href="#">Our Work</a></li>
+		<li><a href="#">Calendar</a></li>
+		<li><a href="#">Search</a></li>
+	</ul>
+</div>
