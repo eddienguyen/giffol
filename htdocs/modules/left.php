45e81c03bdb48b0079f755486db3b88250fb4b26
eddienguyen <eddienguyen996@gmail.com>
2017-08-15 20:11:56 +0700

    modules commit

---
+++ htdocs/modules/left.php
@@ -0,0 +1,10 @@
+
+<div id="left"> 
+    	<ul>
+        	<li> Left 1</li>
+            <li> Left 2</li>
+            <li> Left 3</li>
+            <li> Left 4</li>
+            <li> Left 5</li>
+        </ul>
+</div>
---
+++ htdocs/modules/menu.php
@@ -0,0 +1,10 @@
+
+<div id="menu">
+    	<ul>
+        	<li> HOME </li>
+            <li> Start </li>
+            <li> Products </li>
+            <li> Contact </li>
+            <li> About </li>
+        </ul>
+</div>
